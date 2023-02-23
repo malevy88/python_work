d55c5b0a84aeb58f866d16e28ad07846508a9b6f
Marcus Levy <levyma88@gmail.com>
2023-02-23 14:36:49 -0600

    init commit

---
+++ name.py
@@ -0,0 +1,9 @@
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
