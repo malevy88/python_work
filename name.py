4190adbab5bc325e08d684aad66260a61c8e13b9
Marcus Levy <levyma88@gmail.com>
2023-02-27 15:18:09 -0600

    Added strip, tabs, new line

--- name.py
+++ name.py
@@ -8,3 +8,14 @@ last_name = "lovelace"
 full_name = f"{first_name} {last_name}"
 message = f"Hello, {full_name.title()}!"
 print(message)
+
+print("Python")
+print("\tPython")
+
+print("Languages:\nPython\nC\nJavascript")
+
+print("Languages: \n\tPython\n\tC\n\tJavascript")
+
+favorite_language = 'python '
+favorite_language = favorite_language.rstrip()
+print(favorite_language)
