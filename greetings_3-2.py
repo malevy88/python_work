1851af4ef2d0093f0b65705df58283616ec638f6
Marcus Levy <levyma88@gmail.com>
2023-03-06 08:37:40 -0600

    created new file for exercise 3-2

---
+++ greetings_3-2.py
@@ -0,0 +1,10 @@
+#using names list just created, create personal message for each and print message 
+
+names = ['Deandre', 'Tim', 'Angus', 'Amanda', 'Koppa']
+
+message_0 = f"Hey there {names[0]}, can't wait to go to the movies with you!"
+print(message_0)
+
+message_1 = f"Hey {names[1]}, when is the next time you can hang out?"
+print(message_1)
+
