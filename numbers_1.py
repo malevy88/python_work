cac4f7504c27dc559dbf1cddb70e608121eadace
Marcus Levy <levyma88@gmail.com>
2023-03-06 05:28:55 -0600

    Did example of numbers in Python Crash Course ch1

---
+++ numbers_1.py
@@ -0,0 +1,21 @@
+#integers are whole numbers
+print(2 + 3)
+
+#floats are decimal points
+print(0.1 + 0.1)
+
+#integers & floating points (when dividing any number, result is always floating point. same with mixing the two)
+print(4/2)
+print(1 + 2.0)
+
+#underscores in numbers
+universe_age = 14_000_000_000
+print(universe_age)
+
+#multiple assignment
+x, y, z = 0, 0, 0
+print(x, y, z)
+
+#constants (use all capital letters in Python)
+MAX_CONNECTIONS = 5000
+print(MAX_CONNECTIONS)
