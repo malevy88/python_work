30df9a2e877b9af3036eba5408e810d42f52b34f
Marcus Levy <levyma88@gmail.com>
2023-03-06 14:26:19 -0600

    created motorcycle.py

---
+++ motorcycles.py
@@ -0,0 +1,11 @@
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles.append('ducati') # adds ducati to the end of list
+print(motorcycles)
+
+motorcycles = [] # creates empty list and adds the following to list
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+print(motorcycles)
