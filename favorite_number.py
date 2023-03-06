054506dd1fceeae77a1e67c259d1b4a231280181
Marcus Levy <levyma88@gmail.com>
2023-03-06 05:46:13 -0600

    main

--- favorite_number.py
+++ favorite_number.py
@@ -1,4 +1,7 @@
+#created my favorite number using 2 different variable to represent 00
 favorite_number_1 = 0
 favorite_number_2 = 0
+#created message to showing off favorite number and used f literal to incorporate the two previously created variables
+
 message = f"Hello, my name is Marcus and my favorite number is {favorite_number_1}{favorite_number_2}!"
 print(message)
