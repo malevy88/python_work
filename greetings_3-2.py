31ed74165cfd74c957d8c487f6b64bd8dfcc8f4a
Marcus Levy <levyma88@gmail.com>
2023-03-06 09:44:43 -0600

    created ownList to do exercise 3.3

--- greetings_3-2.py
+++ greetings_3-2.py
@@ -8,3 +8,11 @@ print(message_0)
 message_1 = f"Hey {names[1]}, when is the next time you can hang out?"
 print(message_1)
 
+message_2 = f"{names[2]} I really miss you my dude!"
+print(message_2)
+
+message_3 = f"{names[3]}, I really hope that your dad gets healthy!"
+print(message_3)
+
+message_4 = f"Hey {names[4]}, it was really fun to hang out with you on Saturday!"
+print(message_4)
---
+++ ownList_3.3.py
@@ -0,0 +1,9 @@
+#create and use list to print a series of statements about these cars
+
+cars = ['tesla', 'toyota', 'subaru', 'bmw']
+
+message_1 = f"My dream car would be either a {cars[0].title()} or a {cars[3].upper()}"
+print(message_1)
+
+message_2 = f"In reality though, I can only afford another {cars[1].title()} or a used {cars[2].title()}"
+print(message_2)
