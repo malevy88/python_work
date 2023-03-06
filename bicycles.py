918b5a0229e4c2e2d6b2479889ea5f758aaafa93
Marcus Levy <levyma88@gmail.com>
2023-03-06 08:07:30 -0600

    went through first examples of chapter 3

--- bicycles.py
+++ bicycles.py
@@ -1,4 +1,10 @@
 bicycles = ['trek', 'cannondale', 'redline', 'specailized']
-print(bicycles)
-print(bicycles[0])
-print(bicycles[0].title())
+print(bicycles) #prints the whole list
+print(bicycles[0]) #prints just trek from list
+print(bicycles[0].title()) #prints just trek from list but capitalized
+print(bicycles[1]) #prints cannondale
+print(bicycles[3]) #prints specialized
+print(bicycles[-1]) #-1 always returns the last item in a list
+
+message = f'My first bicycle was a {bicycles[0].title()}'
+print(message)
