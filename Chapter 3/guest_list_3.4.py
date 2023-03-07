4c16e4b3b3ec7e92c8caf04e47bb0f5a2b0a1941
Marcus Levy <levyma88@gmail.com>
2023-03-07 05:48:04 -0600

    created 3-4 for the try it yourself part of python

---
+++ Chapter 3/guest_list_3.4.py
@@ -0,0 +1,7 @@
+guest_list = ['Kobe Bryant', 'Michael Jordan', 'Giannis Antemkoupmo', 'Virgil Abloh', 'Jay Z']
+
+print(f"Hey {guest_list[0]}, where would you like to go get dinner from? Anywhere of your desire!")
+print(f"{guest_list[1]}, what is your favorite restaurant anywhere? We should give it a try together sometime!")
+print(f"{guest_list[2]}, I will definitely get you some Culvers if you can get me an autographed jersey.")
+print(f"Hey {guest_list[3]}, I love the Jordan 4s you did. Let's grab lunch and discuss the design phase")
+print(f"{guest_list[4]}, you're definitely a big inspiration to me. Would love the opportunity to take you out to lunch sometime.")
--- Chapter 3/motorcycles.py
+++ Chapter 3/motorcycles.py
@@ -1,6 +1,8 @@
+#MODIFYING ELEMENTS IN A LIST
 motorcycles = ['honda', 'yamaha', 'suzuki']
 print(motorcycles)
 
+#APPENDING ELEMENTS TO THE END OF THE LIST
 motorcycles.append('ducati') # adds ducati to the end of list
 print(motorcycles)
 
@@ -9,3 +11,48 @@ motorcycles.append('honda')
 motorcycles.append('yamaha')
 motorcycles.append('suzuki')
 print(motorcycles)
+
+#INSERTING ELEMENTS INTO A LIST
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.insert(0, 'ducati') #inserts ducati in the first (0) spot
+print(motorcycles)
+
+#REMOVING AN ITEM USING THE DEL STATEMENT
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+del motorcycles[0] #deletes honda from list (0), used if you know position of item
+print(motorcycles)
+
+#REMOVING AN ITEM USING THE POP() METHOD
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+popped_motorcyles = motorcycles.pop() #pops out last variable in list (suzuki) and assigns it to popped_motorcyles
+print(motorcycles)
+print(popped_motorcyles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+
+last_owned = motorcycles.pop() #pops out suzuki and assigns it to last_owned
+print(f"The last motorcycle that I owned was a {last_owned.title()}")
+
+#POPPING ITEMS FROM ANY POSITION
+first_owned = motorcycles.pop(0) #pops out Honda from the list and assigns it to first_owned
+print(f"The first motorcycle I owned was a {first_owned.title()}.")
+
+#use del when you won't want to use the item from the list anymore. use pop when you will want to
+
+#REMOVING ITEM BY VALUE
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+motorcycles.remove('ducati') #removes ducati item from list
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
