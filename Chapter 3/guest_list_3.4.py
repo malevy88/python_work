ceab1cde12e8dbdb582f6395fb165df168f0ca80
Marcus Levy <levyma88@gmail.com>
2023-03-08 05:26:48 -0600

    replaced guest_list

--- Chapter 3/guest_list_3.4.py
+++ Chapter 3/guest_list_3.4.py
@@ -1,7 +1,7 @@
 #if you could invite anyone, living or deceased to dinner, who would you invite? Make a list that includes at least 3 people
 #you'd like to invite to dinner. Then use your list to prinmt a message to each person, inviting them to dinner.
 
-
+guest_list = ['Kobe Bryant', 'Michael Jordan', 'Giannis Antemkoupmo', 'Virgil Abloh', 'Jay Z']
 print(f"Hey {guest_list[0]}, where would you like to go get dinner from? Anywhere of your desire!")
 print(f"{guest_list[1]}, what is your favorite restaurant anywhere? We should give it a try together sometime!")
 print(f"{guest_list[2]}, I will definitely get you some Culvers if you can get me an autographed jersey.")
