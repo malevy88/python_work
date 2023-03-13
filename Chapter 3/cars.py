890b55459348b862e9e28a830d1f299ca65418a0
Marcus Levy <levyma88@gmail.com>
2023-03-13 11:16:14 -0500

    commit

--- Chapter 3/cars.py
+++ Chapter 3/cars.py
@@ -6,3 +6,12 @@ cars = ['bmw', 'audi', 'toyota', 'subaru']
 cars.sort(reverse=True) #changes order of list to reverse alphbetical order
 print(cars)
 
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print("Here is the original list")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars)) #prints a temporary sorted list
+
+print("\nHere is the original list again:")
+print(cars)
