a481a4279b9048426dd5dbd6c936979d9658c91a
Marcus Levy <levyma88@gmail.com>
2023-03-13 10:57:20 -0500

    created cars.py

---
+++ Chapter 3/cars.py
@@ -0,0 +1,8 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort() #changes order of list to alphabetical order
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort(reverse=True) #changes order of list to reverse alphbetical order
+print(cars)
+
