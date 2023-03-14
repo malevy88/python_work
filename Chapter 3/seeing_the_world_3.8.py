60f175eb6ce61c9760e1ce2b8ca4384262ae34dd
Marcus Levy <levyma88@gmail.com>
2023-03-14 05:19:36 -0500

    Working through 3.8

--- Chapter 3/seeing_the_world_3.8.py
+++ Chapter 3/seeing_the_world_3.8.py
@@ -1,4 +1,4 @@
-#think of at least 5 different places in the world you'd like to visit
+#think of at least 5 different places in the world you'd like to visit 
 #store locations in a list. make sure list is not in alphabetical order
 #print list in original order.
 #use sorted() to print list in alphabetical order without modifying actual list
@@ -7,3 +7,18 @@
 #user reverse to change list back to original order and print
 #use sort() to change list to alphbetical order and print
 #use sort() to store list as reverse-alphabetical order and print.
+
+travel = ['japan', 'amsterdam', 'italy', 'united kingdom', 'france']
+print(travel)
+
+print(sorted(travel)) #prints list in alphabetical order without changing order of original list
+print(travel)
+
+travel.reverse() #reverses order of list (not alphabetical)
+print(travel) #prints reversed list
+
+travel.reverse() #reverses the list back to the original order
+print(travel) #prints in original order again
+
+travel.sort() #sorts list in alphabetical order
+print(travel) #prints alphabetized list
