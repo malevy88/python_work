f5d403cd7883c36f4b56910e78b850c214a77c4d
Marcus Levy <levyma88@gmail.com>
2023-03-14 05:33:44 -0500

    Created 3.10 and added what 3.9 need to acheive

--- Chapter 3/dinner_guests_3.9.py
+++ Chapter 3/dinner_guests_3.9.py
@@ -1,3 +1,5 @@
+#working with one of the programs from exercise 3.4-3.7, use len() to print a message indicating the number of people you're inviting to dinner
+
 guest_list = ['Kobe Bryant', 'Michael Jordan', 'Giannis Antemkoupmo', 'Virgil Abloh', 'Jay Z']
 
 print("\nI invited the following number of guests to dinner:") 
