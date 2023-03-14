e3f2735ee79ed875441ab893fe2fd027ee9caa14
Marcus Levy <levyma88@gmail.com>
2023-03-14 05:28:56 -0500

    Finished 3.9

---
+++ Chapter 3/dinner_guests_3.9.py
@@ -0,0 +1,4 @@
+guest_list = ['Kobe Bryant', 'Michael Jordan', 'Giannis Antemkoupmo', 'Virgil Abloh', 'Jay Z']
+
+print("\nI invited the following number of guests to dinner:") 
+print(len(guest_list))
