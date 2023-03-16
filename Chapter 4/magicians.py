5842de057081de9ca09f9731d5863b182b25c26c
Marcus Levy <levyma88@gmail.com>
2023-03-16 05:34:44 -0500

    Beginning of Chapter 4

---
+++ Chapter 4/magicians.py
@@ -0,0 +1,18 @@
+#for loop intro
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+
+#print message to each in a for loop
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+
+#prints out both messages for each magician (cycles through both until program/loop is done)
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n") # \n inserts the line break between loops
+
+print("Thanks you, everyone. That was a great magic show!") #prints after for loop since not indented (part of)
+
