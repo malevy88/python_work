97168a969f9b142b81774357c938b6ea37a23731
Marcus Levy <levyma88@gmail.com>
2023-03-18 07:48:57 -0500

    Finished 4.2 exercise

---
+++ Chapter 4/animals_4.2.py
@@ -0,0 +1,11 @@
+# think of at least 3 different animals that have a common characteristic
+# store the names of these animals in a list
+# use a for loop to print oiut the name of each
+# print a statement about each animal
+# add a line at the end of program stating what they have in common
+
+good_pets = ['cat', 'turtle', 'hamster']
+for pet in good_pets:
+    print(f"I think that a {pet} would be a great addition to the house.")
+
+print('Any of these animals would make great pets!')
