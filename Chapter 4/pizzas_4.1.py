a5b493e16c12d23491943379ab8b2caa2eb75eca
Marcus Levy <levyma88@gmail.com>
2023-03-18 07:36:23 -0500

    Finished 4.1 challenge

---
+++ Chapter 4/pizzas_4.1.py
@@ -0,0 +1,14 @@
+"""
+think of at least 3 kinds of your favorite pizza.
+store these pizza names in a list, and then use a for loop to print the name of each
+modify you for loop to print a sentence using the name of the pizza, instead of printing just the name of the pizza
+for each, you should have one line of output containing a simple statement like "I like pepperoni pizza"
+make a line at the end of your program, outside the loop, that states how much you like pizza. The output should consist
+of three or more line about the kinds of pizza you like and then an additional sentence, such as I really love pizza!
+"""
+
+favorite_pizzas = ['sausage pizza', 'pepperoni pizza', 'cheese pizza']
+for pizza in favorite_pizzas:
+    print(f"I like {pizza}. It's one of my favorites")
+
+print("I really just love pizza!!")
