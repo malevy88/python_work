4b79b24f913fe853ab6475ab3f7d046f4cbad186
Marcus Levy <levyma88@gmail.com>
2023-03-20 05:30:47 -0500

    Completed 4.6

---
+++ Chapter 4/odd_numbers_4.6.py
@@ -0,0 +1,6 @@
+#use the third argument of the range() function to make a list of odd numbers from 1 to 20.
+#use a for loop to print each number
+
+odd_numbers = list(range(1, 20, 2))
+for number in odd_numbers:
+    print(number)
