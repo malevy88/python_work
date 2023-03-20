7d390fb89c1b24324fcdbbb868517d4529671067
Marcus Levy <levyma88@gmail.com>
2023-03-20 05:19:42 -0500

    Completed 4.4

---
+++ Chapter 4/one_million_4.4.py
@@ -0,0 +1,5 @@
+#make a list of the number from one to one million, then use a for lopp to print the numbers.
+#(if the output is taking too long, stop it by pressiong the CTRL-C or by closing the output window)
+
+one_million = list(range(1, 1000001))
+print(one_million)
