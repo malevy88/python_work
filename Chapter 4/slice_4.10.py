f5125daaf6358a7a7cb37af6f8226b1e81e9f4b2
Marcus Levy <levyma88@gmail.com>
2023-03-22 05:46:02 -0500

    Completed 4.10 slicing exercise

---
+++ Chapter 4/slice_4.10.py
@@ -0,0 +1,15 @@
+#using one of the programs written in this chapter
+# 1. print the message "the first 3 items in the list are:". Then use a slice to print the first three items from that programs list
+# 2. Do the same for the 3 middle of the list
+# 3. Do the same for the last 3 items of the list
+
+my_foods = ['pizza', 'swedish meatballs', 'pasta', 'bbq', 'brats', 'polishes', 'chantlilly cake' 'sweet tea', 'turkey sandwich']
+
+print("The first three items on the list are:")
+print(my_foods[0:3])
+
+print("Three items from the middle of the list are:")
+print(my_foods[3:6])
+
+print("The last three items on the list are:")
+print(my_foods[6:9])
