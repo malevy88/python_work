9a009d824b360af2d7757ac0ebbe960ec1a53ae1
Marcus Levy <levyma88@gmail.com>
2023-04-03 05:55:11 -0500

    Changed the line length to 80

--- Chapter 4/buffet_4.13.py
+++ Chapter 4/buffet_4.13.py
@@ -1,6 +1,11 @@
-# a buffet style restuarant offers only five basic foods. think of five basic foods and store them in a tuple.
-# use a for loop to print each food the restaurant offers. try to modify one of the items and make sure that Python rejects the change
-# the restaurant changes its menu, replacing two of the items with different foods. add a line that rewrites the tuple, then use a for loop
+# a buffet style restuarant offers only five basic foods. think of five basic 
+# foods and store them in a tuple.
+
+# use a for loop to print each food the restaurant offers. try to modify one of 
+# the items and make sure that Python rejects the change.
+
+# the restaurant changes its menu, replacing two of the items with different 
+# foods. add a line that rewrites the tuple, then use a for loop
 # to print each of the item on the revised menu
 
 buffet_menus = ("hot dog", "pizza", "chicken tenders", "buttered noodles", "chicken noodle soup", "fries")
