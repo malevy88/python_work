fbe10d77fbac097bd8483a13cf58d9a12605a05f
Marcus Levy <levyma88@gmail.com>
2023-04-03 07:31:07 -0500

    Changed to comply with PEP 8

---
+++ Chapter 4/code_review_4.15.py
@@ -0,0 +1,8 @@
+# choose 3 of the programs you've written in this chapter and modify each one to
+# comply with PEP 8.  Set text editor to insert four spaces every time you
+# press the TAB key if you havn'e done so already. Use less than 80 characters 
+# on each line and set your editor to show a veritcal guideline at the 80th 
+# character position. Don't use blank lines excessively in your program files.
+
+# check magicians.py for PEP 8 compliance
+# check
