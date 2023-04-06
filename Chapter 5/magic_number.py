dfdbf8ae0cfe27d6a3a428f3711e2a1c0ea3f292
Marcus Levy <levyma88@gmail.com>
2023-04-06 11:21:06 -0500

    numerical comparisons

---
+++ Chapter 5/magic_number.py
@@ -0,0 +1,12 @@
+age = 18
+print(age == 18)
+
+answer = 17
+if answer != 42:
+    print("This is not the correct answer. Please try again!")
+
+age = 19
+print(age < 21)
+print(age <= 21)
+print(age > 21)
+print(age >= 21)
