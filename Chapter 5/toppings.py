aaf1862aa07e43ec3961519011bcc73215f460eb
Marcus Levy <levyma88@gmail.com>
2023-04-06 13:45:45 -0500

    Added checking values in lists example

--- Chapter 5/toppings.py
+++ Chapter 5/toppings.py
@@ -4,3 +4,12 @@ requested_topping = "mushrooms"
 
 if requested_topping != 'anchovies':
     print("Hold the anchovies!")
+
+
+# checking whether a value is in a list
+
+requested_toppings = ['mushrooms', 'onions', 'pineapple']
+#true since mushrooms are on the list
+print('mushrooms' in requested_toppings) 
+#false since pepperoni is not on the list
+print('pepperoni' in requested_toppings)
