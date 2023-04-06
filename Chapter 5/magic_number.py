316162a5a7bf5e64b73ee3ffa5e93addc617c045
Marcus Levy <levyma88@gmail.com>
2023-04-06 12:09:57 -0500

    added comments

--- Chapter 5/magic_number.py
+++ Chapter 5/magic_number.py
@@ -1,3 +1,5 @@
+#numerical comparisons
+
 age = 18
 print(age == 18)
 
@@ -6,7 +8,23 @@ if answer != 42:
     print("This is not the correct answer. Please try again!")
 
 age = 19
-print(age < 21)
-print(age <= 21)
-print(age > 21)
-print(age >= 21)
+print(age < 21) #returns true
+print(age <= 21) #returns true
+print(age > 21)  #returns false
+print(age >= 21) #returns false
+
+age_0 = 22
+age_1 = 18
+
+# both conditions have to be met with AND to return a true statement
+print(age_0 >= 21 and age_1 >= 21) #returns false
+age_1 = 22
+print(age_0 >= 21 and age_1 >= 21) #returns true
+
+# one condition has to be met for OR condition to return true statement
+age_0 = 22
+age_1 = 18
+print(age_0 >= 21 or age_1 >= 21) #returns true
+
+age_0 = 18
+print(age_0 >= 21 or age_1 >= 21) #returns false
