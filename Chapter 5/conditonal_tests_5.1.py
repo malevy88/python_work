096d77dd8ffebaba93e8f00c2b59f0f2b09736fc
Marcus Levy <levyma88@gmail.com>
2023-04-06 14:35:07 -0500

    Created 5.1

---
+++ Chapter 5/conditonal_tests_5.1.py
@@ -0,0 +1,12 @@
+# write a series of conditonal tests.
+# print a statement describing each test and your prediction for each test.
+# create at least 10 tests. have at least 5 evaluate true and 5 false
+
+# example 1
+favorite_shoe = 'nike'
+print('Is my favorite shoe == nike? I believe that will result in true!')
+print(favorite_shoe == 'nike')
+
+# example 2
+favorite_soda = 'coke'
+print("What do you think my favoite soda is going to be?" "Do you think it's root")
