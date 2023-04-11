f267be7a57e19bb6b4f3fa2317124adc097ebd2d
Marcus Levy <levyma88@gmail.com>
2023-04-11 05:42:06 -0500

    Finished 5.1 exercise (conditional tests)

--- Chapter 5/conditonal_tests_5.1.py
+++ Chapter 5/conditonal_tests_5.1.py
@@ -18,4 +18,22 @@ print(favorite_soda == 'coke')
 
 # example 3
 favorite_color = 'black'
-print()
+print("\nDo you think my favorite color is blue?")
+print(favorite_color == 'blue')
+print("\nActually, if you guessed black, that would be true!!")
+print(favorite_color == 'black')
+
+# example 4
+current_car = 'Kia Seltos'
+print("\nDo you still think I drive a Toyota Matrix?")
+print(current_car == 'Toyota Matrix')
+print("\nIf you would have guess that the Kia is mine, you're right")
+print(current_car == "Kia Seltos")
+
+# example 5
+dinner_tonight = "spaghetti and meatballs"
+print("\nGuess what we're having for dinner tonight?")
+print(dinner_tonight == 'swedish meatballs')
+print("No, we're not doing that. Take another guess")
+print(dinner_tonight == 'spaghetti and meatballs')
+print("Yep, that's it! Spaghetti and meatballs it is!")
