fa153ee99862dcca4658c9c314955f145119204b
Marcus Levy <levyma88@gmail.com>
2023-04-12 09:56:16 -0500

    Started exercise 5.2

---
+++ Chapter 5/more_conditional_tests_5.2.py
@@ -0,0 +1,12 @@
+# test for equality and inequality with strings
+# test using the lower() method
+# numerical tests involving equality/inequality, greater than and less than,
+# greater than or equal to, and less than or equal to
+# tests using the and keyword and the or keyword
+# test whether an item is in a list
+# test whether an item is not in a list
+
+full_name = 'marcus allen'
+
+print('Is Marcus allen equal to marcus allen')
+print(full_name == 'Marcus Allen')
