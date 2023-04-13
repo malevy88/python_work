ea8ae4e118d03bcfe4f8d70a2322b93241737232
Marcus Levy <levyma88@gmail.com>
2023-04-13 12:18:03 -0500

    Added greater than/equal to example

--- Chapter 5/more_conditional_tests_5.2.py
+++ Chapter 5/more_conditional_tests_5.2.py
@@ -1,18 +1,24 @@
-# test for equality and inequality with strings
-# test using the lower() method
+# test for equality and inequality with strings ✅
+# test using the title() method ✅
 # numerical tests involving equality/inequality, greater than and less than,
 # greater than or equal to, and less than or equal to
-# tests using the and keyword and the or keyword
+# tests using the AND keyword and the OR keyword
 # test whether an item is in a list
 # test whether an item is not in a list
 
-
+# string and title example
 full_name = 'marcus allen'
-print('Is Marcus allen equal to marcus allen')
+print('Is Marcus Allen equal to marcus allen')
 print(full_name == 'Marcus Allen')  # false since not capitalized
 print("It appears that it isn't!")
-
-
 print("\nWhat if we changed it to uppercase?")
 new_full_name = full_name.title()  # changes it to capitalized
 print(new_full_name == 'Marcus Allen')  # returns true
+
+# greater than or equal to example
+age_0 = 35
+age_1 = 36
+print("\nWhat will 35 >= 36 result as? True or false")
+print(age_0 >= age_1)
+print("What about reversed? 35 <= 36?")
+print(age_0 <= age_1)
