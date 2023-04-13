6592e00f95ffb5a16f09f5939052dd535970993e
Marcus Levy <levyma88@gmail.com>
2023-04-13 11:54:26 -0500

    Added comments

--- Chapter 5/more_conditional_tests_5.2.py
+++ Chapter 5/more_conditional_tests_5.2.py
@@ -6,12 +6,13 @@
 # test whether an item is in a list
 # test whether an item is not in a list
 
-full_name = 'marcus allen'
 
+full_name = 'marcus allen'
 print('Is Marcus allen equal to marcus allen')
-print(full_name == 'Marcus Allen')
+print(full_name == 'Marcus Allen')  # false since not capitalized
 print("It appears that it isn't!")
 
+
 print("\nWhat if we changed it to uppercase?")
-new_full_name = full_name.title()
-print(new_full_name)
+new_full_name = full_name.title()  # changes it to capitalized
+print(new_full_name == 'Marcus Allen')  # returns true
