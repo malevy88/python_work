26e8b157f01128c646e4c7b3acae640e601718c4
Marcus Levy <levyma88@gmail.com>
2023-04-17 09:06:57 -0500

    Finished examples

--- Chapter 5/amusement_park.py
+++ Chapter 5/amusement_park.py
@@ -21,3 +21,31 @@ else:
     price = 40
 
 print(f"Your admission cost is ${price}.")
+
+# adding in a discount for seniors
+age = 80
+
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+elif age < 65:
+    price = 40
+else:
+    price = 20
+
+print(f"Your admission cost is ${price}.")
+
+# omitting the else block
+age = 12
+
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+elif age < 65:
+    price = 40
+elif age >= 65:
+    price = 20
+
+print(f"Your admission cost is ${price}")
--- Chapter 5/toppings.py
+++ Chapter 5/toppings.py
@@ -5,11 +5,37 @@ requested_topping = "mushrooms"
 if requested_topping != 'anchovies':
     print("Hold the anchovies!")
 
-
 # checking whether a value is in a list
 
 requested_toppings = ['mushrooms', 'onions', 'pineapple']
-#true since mushrooms are on the list
-print('mushrooms' in requested_toppings) 
-#false since pepperoni is not on the list
+# true since mushrooms are on the list
+print('mushrooms' in requested_toppings)
+# false since pepperoni is not on the list
 print('pepperoni' in requested_toppings)
+
+
+# testing multiple conditions
+requested_toppings = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requested_toppings:
+    print("\nAdding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza!")
+
+# testing with if/elif argument
+requested_toppings = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requested_toppings:
+    print("\nAdding mushrooms.")
+# doesn't get added since no in toppings list
+elif 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+# doesn't get added since argument for mushrooms passed, skipping over cheese
+elif 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza!")
