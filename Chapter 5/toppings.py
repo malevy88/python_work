c51b87284b402a886b166099031f55a5ccfbc41f
Marcus Levy <levyma88@gmail.com>
2023-04-19 09:57:31 -0500

    updated with if statements in lists

--- Chapter 5/toppings.py
+++ Chapter 5/toppings.py
@@ -39,3 +39,27 @@ elif 'extra cheese' in requested_toppings:
     print("Adding extra cheese.")
 
 print("\nFinished making your pizza!")
+
+# checking for special items
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    # 86'd green peppers so prints statement if those are added
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right now.")
+    else:
+        print(f"Adding {requested_topping}.")
+
+print("\nFinished making your pizza!")
+
+# checking that a list is not empty
+requested_toppings = []
+
+# runs the conditional test to see if list is empty, returns false so executes
+# the else statement
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print(f"Adding {requested_topping}.")
+    print("\nFinished making your pizza!")
+else:
+    print("Are you sure you just want a plain pizza?")
