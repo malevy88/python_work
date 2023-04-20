a76f41829e8cd3025dd4bc8677e42510d791e966
Marcus Levy <levyma88@gmail.com>
2023-04-20 09:13:39 -0500

    created 5.10 exercise

---
+++ Chapter 5 (If Statements)/checking_usernames_5.10.py
@@ -0,0 +1,14 @@
+# make a list of five or more usernames called current_users
+# make another list of five usernames called new_users. Make sure one or two of
+# new usernames are also in the current_users list.
+# loop through the new_users list to see if each new username has already been
+# used. If it has, print a message that the person will need to enter a new
+# username. If a username has not bee used, print a messhae saying the username
+# is available. Make sure the your comparison is case insensitive. If 'John'
+# has been used, 'JOHN' should not be accepted. (To do this, you'll need to make
+# a copy of current_users containing the lowercase versions of all exisiting
+# users.)
+
+current_users = ['jon', 'marcus', 'malevy', 'tommy', 'lindsey']
+
+new_users = ['shirley', 'marcus', 'katrina', 'marco', 'lindsey']
