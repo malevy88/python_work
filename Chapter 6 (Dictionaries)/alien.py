062285831d66b5100823e8258a5ed297bec321cb
Marcus Levy <levyma88@gmail.com>
2023-04-26 08:19:27 -0500

    Created alien and favorite language exercises

--- Chapter 6 (Dictionaries)/alien.py
+++ Chapter 6 (Dictionaries)/alien.py
@@ -3,3 +3,56 @@ alien_0 = {'color': 'green', 'points': 5}
 
 print(alien_0['color'])
 print(alien_0['points'])
+
+# accessing values in a dictionary
+alien_0 = {'color': 'blue', 'points': 10}
+
+new_points = alien_0['points']
+print(f"You just earned {new_points} points!")
+
+# adding new key values
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+# dictionaries retains order they were defined in
+print(alien_0)
+
+# starting with an empty dictionary
+alien_0 = {}
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+
+print(alien_0)
+
+# modifying values in a dictionary
+alien_0 = {'color': 'green'}
+print(f"The alien is {alien_0['color']}.")
+
+alien_0['color'] = 'yellow'
+print(f"The alien is now {alien_0['color']}.")
+
+# track the position at different speeds
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print(f"Original position: {alien_0['x_position']}")
+alien_0['speed'] = 'fast'
+# move the alien to the right.
+# determine how far to move the alien based on its current speed.
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    # this must be a fast alien.
+    x_increment = 3
+
+# the new position is the old position plus the increment.
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print(f"New position: {alien_0['x_position']}")
+
+# removing key value pairs
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+del alien_0['points']  # removed permanently
+print(alien_0)
---
+++ Chapter 6 (Dictionaries)/favorite_languages.py
@@ -0,0 +1,10 @@
+# dictionary of similar objects
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'python',
+}
+
+language = favorite_languages['sarah'].title()
+print(f"Sarah's favorite language is {language}.")
