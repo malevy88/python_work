40445ae46a315b8452030cf69a00dfd5731e9841
Marcus Levy <levyma88@gmail.com>
2023-05-01 08:00:36 -0500

    Exercise created

---
+++ Chapter 6 (Dictionaries)/alien_no_points.py
@@ -0,0 +1,11 @@
+# using get() to access values
+
+'''
+# results in traceback error since points don't exist
+alien_0 = {'color': 'green', 'speed': 'slow'}
+print(alien_0['points'])
+'''
+
+alien_0 = {'color': 'green', 'speed': 'slow'}
+point_value = alien_0.get('points', 'No point value assigned.')
+print(point_value)
