19938c5addd9cac4938a6a703ec85bda36b346b9
Marcus Levy <levyma88@gmail.com>
2023-05-01 08:37:39 -0500

    Created exercise 6.2

---
+++ Chapter 6 (Dictionaries)/favorite_numbers_6.2.py
@@ -0,0 +1,5 @@
+# use a dictionary to store people's favorite numbers.
+# think of five names, and use them as keys in your dictionary.
+# think of a favorite number for each person, and store each as a value in your
+# dictionary. Print each person's name and thier favorite number. For even more
+# fun, poll a few freinds and get some actual data for your program.
