34617dcf40d72ca5b4789f05f6cb4a8077fcfa2f
Marcus Levy <levyma88@gmail.com>
2023-05-01 08:34:24 -0500

    Completed 6.1

---
+++ Chapter 6 (Dictionaries)/person_6.1.py
@@ -0,0 +1,16 @@
+# use a dictionary to store information about a person you know.
+# store their first name, last name, age, and the city in which they live.
+# you should have keys such as first_name, last_name, etc.
+# print each piece of information stored in your dictionary
+
+best_friend = {
+    'first name': 'Deandre',
+    'last_name': 'Wilson',
+    'age': 33,
+    'city': 'Milwaukee',
+}
+
+print(best_friend['first name'])
+print(best_friend['last_name'])
+print(best_friend['city'])
+print(best_friend['age'])
