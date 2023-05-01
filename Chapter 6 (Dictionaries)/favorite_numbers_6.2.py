286a726245df98981d081e0ad408762ba104df69
Marcus Levy <levyma88@gmail.com>
2023-05-01 08:55:16 -0500

    Completed exercise 6.2

--- Chapter 6 (Dictionaries)/favorite_numbers_6.2.py
+++ Chapter 6 (Dictionaries)/favorite_numbers_6.2.py
@@ -3,3 +3,17 @@
 # think of a favorite number for each person, and store each as a value in your
 # dictionary. Print each person's name and thier favorite number. For even more
 # fun, poll a few freinds and get some actual data for your program.
+
+favorite_numbers = {
+    'Deandre': 5,
+    'Marcus': 00,
+    'Lindsey': 24,
+    'Amanda P': 4,
+    'Angus': 100,
+}
+
+print(f"Deandre's favorite number is {favorite_numbers['Deandre']}.")
+print(f"Marcus' favorite number is {favorite_numbers['Marcus']}.")
+print(f"Lindsey's favorite number is {favorite_numbers['Lindsey']}.")
+print(f"Amanda's favorite number is {favorite_numbers['Amanda P']}.")
+print(f"Angus' favorite number is {favorite_numbers['Angus']}.")
