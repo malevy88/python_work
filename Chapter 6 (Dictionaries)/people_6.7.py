04b8856911b6fa464b66dbac7fa9ecdfb5d198d4
Marcus Levy <levyma88@gmail.com>
2023-05-05 11:59:34 -0500

    Created 6.7 exercise, formatted 6.1

---
+++ Chapter 6 (Dictionaries)/people_6.7.py
@@ -0,0 +1,28 @@
+# start with the program you wrote for 6.1.
+# make two new dictionaries representing different people, and store all three
+# dictionaries in a list called people.
+# loop through your list of people.
+# as you loop through the list, print everything you know about each person.
+
+friend_0 = {
+    'first_name': 'Deandre',
+    'last_name': 'Wilson',
+    'age': 33,
+    'city': 'Milwaukee',
+}
+
+friend_1 = {
+    'first_name': 'Tim',
+    'last_name': 'Callen',
+    'age': 31,
+    'city': 'Wauwatosa',
+}
+
+friend_2 = {
+    'first_name': 'Angus',
+    'last name': 'Barsch',
+    'age': 31,
+    'city': 'Los Angeles',
+}
+
+people = [friend_0, friend_1, friend_2]
