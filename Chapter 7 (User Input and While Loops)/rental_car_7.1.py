998ae7b2dd55dfeaeb4435ce0f8a294e99a7ddb5
Marcus Levy <levyma88@gmail.com>
2023-05-10 14:07:03 -0500

    Created exercise 7.1 and two examples

---
+++ Chapter 7 (User Input and While Loops)/rental_car_7.1.py
@@ -0,0 +1,7 @@
+# write a prgram that asks the user what kind of rental car they would like.
+# print a message about that car, such as "let me see if I can find a subaru."
+
+
+message = input("What kind of car is is that you're interested in today? ")
+
+print(f"Let me see if I can find a {message} for you today. Be right back.")
