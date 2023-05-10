c8455ee38754c78a0029bc30045352bf50edeab6
Marcus Levy <levyma88@gmail.com>
2023-05-10 13:38:42 -0500

    Follow along

--- Chapter 7 (User Input and While Loops)/greeter.py
+++ Chapter 7 (User Input and While Loops)/greeter.py
@@ -11,3 +11,14 @@ prompt += "\nWhat is your first name? "
 # passes prompt variable through name
 name = input(prompt)
 print(f"\nHello, {name}!")
+
+
+# using int() to accept numerical input
+
+age = input("How old are you? ")  # stores age as string, not int
+print(f"\nYou are {age} years old?")
+
+age = input("How old are you? ")
+age = int(age)  # stores age as an integer
+print(age)
+print(age >= 18)  # prints true if entered age is older than 18
