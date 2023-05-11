447ec0ea89597ecf65f45a16be7e5a853761346c
Marcus Levy <levyma88@gmail.com>
2023-05-11 12:14:36 -0500

    Created examples

--- Chapter 7 (User Input and While Loops)/parrot.py
+++ Chapter 7 (User Input and While Loops)/parrot.py
@@ -2,3 +2,17 @@
 
 message = input("Tell me something, and I will repeat it back to you: ")
 print(message)
+
+
+# while loop in action
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "  # letting use choose to quit
+
+message = ""
+while message != 'quit':
+    message = input(prompt)
+
+    if message != 'quit':
+        print(message)
+
+# using a flag
