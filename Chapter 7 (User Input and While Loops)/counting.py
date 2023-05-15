dd3198bcb34a45b510642b4ded31bf6dca32a62c
Marcus Levy <levyma88@gmail.com>
2023-05-15 13:59:03 -0500

    added inifinite loop exercise

--- Chapter 7 (User Input and While Loops)/counting.py
+++ Chapter 7 (User Input and While Loops)/counting.py
@@ -1,6 +1,21 @@
 # introducing while loops
-
 current_number = 1
 while current_number <= 5:
     print(current_number)
     current_number += 1
+
+# using continue in a loop
+current_number = 0
+while current_number < 10:
+    current_number += 1
+    if current_number % 2 == 0:
+        continue
+
+    print(current_number)
+
+# avoiding infinite loop
+
+# this loops runs forever!
+x = 1
+while x <= 5:
+    print(x)
