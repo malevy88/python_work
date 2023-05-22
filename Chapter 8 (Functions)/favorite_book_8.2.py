a6c358bc3b7b037230e698258d78bcff8a560fc3
Marcus Levy <levyma88@gmail.com>
2023-05-22 09:58:13 -0500

    Completed 8.2

---
+++ Chapter 8 (Functions)/favorite_book_8.2.py
@@ -0,0 +1,10 @@
+# write a function called favorite_book() that accepts one parameter, title.
+# the function should print a message, such as one of my favorite books is
+# alice in wonderland. call the function, making sure to include a book title as
+# an argument in the function call.
+
+def favorite_book(title):
+    print(f"My favorite book is {title.title()}.")
+
+
+favorite_book("Alice in Wonderland")
