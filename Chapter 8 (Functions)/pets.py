4efa800952eef2caeb5a0738738c8eff53883caf
Marcus Levy <levyma88@gmail.com>
2023-05-22 13:31:34 -0500

    added keyword arguments

---
+++ Chapter 8 (Functions)/pets.py
@@ -0,0 +1,32 @@
+# passing arguments
+
+# positional arguments (need to be same order the parameters were written)
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+
+describe_pet('hamster', 'harry')
+
+
+# multiple function calls
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+
+describe_pet('hamster', 'harry')
+describe_pet('dog', 'willie')
+
+# keyword arguments
+
+
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+
+describe_pet(animal_type='hamster', pet_name='harry')
