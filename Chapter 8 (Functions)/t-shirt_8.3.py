43409e28d831234d17e51bdd458de0fdb459067a
Marcus Levy <levyma88@gmail.com>
2023-05-23 12:14:52 -0500

    completed 8.3 with both functions

--- Chapter 8 (Functions)/t-shirt_8.3.py
+++ Chapter 8 (Functions)/t-shirt_8.3.py
@@ -2,6 +2,10 @@
 # message that should be printed on the shirt. the function should print a
 # sentence summarizing the size of the shirt and the message printed on it.
 
+# call the function once using positional arguments to make a shirt.
+# call the function a second time using keyword arguments.
+
+
 def make_shirt(size, text):
     """Display information about a shirt."""
     print("\nI'm going to make a " + size + " t-shirt.")
@@ -9,3 +13,4 @@ def make_shirt(size, text):
 
 
 make_shirt('XXXL', 'Hidden Characters')
+make_shirt(size='XXXL', text='Hidden Characters')
