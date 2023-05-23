17b881e8a20380aed2263aa130b485c11270aa6a
Marcus Levy <levyma88@gmail.com>
2023-05-23 11:58:32 -0500

    worked through pet example

--- Chapter 8 (Functions)/pets.py
+++ Chapter 8 (Functions)/pets.py
@@ -30,3 +30,30 @@ def describe_pet(animal_type, pet_name):
 
 
 describe_pet(animal_type='hamster', pet_name='harry')
+
+# default values
+
+
+def describe_pet(pet_name, animal_type='dog'):  # added type as default value
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+
+# since type is default value, don't need it here
+describe_pet(pet_name='willie')
+# could also use this
+describe_pet('willie')
+# to describe animal other than dog, needs to be listed after default value
+describe_pet(pet_name='harry', animal_type='hamster')
+
+# avoiding arugument errors
+
+
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+
+describe_pet()  # missing arguments
