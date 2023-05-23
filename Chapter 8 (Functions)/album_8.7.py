dba46fcda39f07e1331729569bd44aedce06752b
Marcus Levy <levyma88@gmail.com>
2023-05-23 15:13:57 -0500

    started 8.7 exercise

---
+++ Chapter 8 (Functions)/album_8.7.py
@@ -0,0 +1,11 @@
+# write a function called make_album() that builds a dictionary describing
+# a music album. The function should take an artist name and an album name. If
+# the calling line includes a value for the number of songs, add that value to
+# the album's dictionary. Make at least one new function call that includes the
+# number of songs on an album
+
+def make_album(artist, album, song_number= ''):
+    if song_number = :
+        print(
+            f"\nThe album is by {artist}, from the album {album} and 
+            has {song_number} on it. )
