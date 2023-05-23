e54d4d655ad9754b167c68621d5d6481a2c37b57
Marcus Levy <levyma88@gmail.com>
2023-05-23 13:59:00 -0500

    added age to function

--- Chapter 8 (Functions)/person.py
+++ Chapter 8 (Functions)/person.py
@@ -8,3 +8,17 @@ def build_person(first_name, last_name):
 
 musician = build_person('james', 'maynard')
 print(musician)
+
+# stores a person's age as well
+
+
+def build_person(first_name, last_name, age=None):
+    """Return a dictionary of information about a person"""
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+    return person
+
+
+musician = build_person('james', 'maynard', age=59)
+print(musician)
