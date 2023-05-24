e609ea05305a6189aa70323887795510a61c288d
Marcus Levy <levyma88@gmail.com>
2023-05-24 14:53:15 -0500

    created examples

---
+++ Chapter 8 (Functions)/greet_users.py
@@ -0,0 +1,11 @@
+# passing a list to a function
+
+def greet_users(names):
+    """Print a simple greeting to each user in the list."""
+    for name in names:
+        msg = "Hello, " + name.title() + "!"
+        print(msg)
+
+
+usernames = ['hannah', 'ty', 'margot']
+greet_users(usernames)
