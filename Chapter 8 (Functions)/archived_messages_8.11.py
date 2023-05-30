1e89b14cbc158d7f1105ecd9cb9deb17d0a2345c
Marcus Levy <levyma88@gmail.com>
2023-05-30 10:03:21 -0500

    completed exercise 8.11

--- Chapter 8 (Functions)/archived_messages_8.11.py
+++ Chapter 8 (Functions)/archived_messages_8.11.py
@@ -4,18 +4,25 @@ a copy of the list of messages. After calling the function, print both of your
 lists to show that the original list has retained its messages.
 """
 
-messages = ['hello', 'how are you?', 'what are you doing?', 'goodbye']
-sent_messages = []
 
-
-def send_messages(messages):
+def send_messages(messages, sent_messages):
     while messages:
-        current_message = messages.pop()
+        current_message = messages.pop(0)
         print(f"Sending message: {current_message}")
         sent_messages.append(current_message)
 
 
-send_messages(messages[:])
-print("\nFinal lists:")
+def show_sent_messages(sent_messages):
+    print("\nThe following messages have been sent:")
+    for sent_message in sent_messages:
+        print(sent_message)
+
+
+messages = ['hello', 'how are you?', 'what are you doing?', 'goodbye']
+sent_messages = []
+
+send_messages(messages[:], sent_messages)
+show_sent_messages(sent_messages)
+
 print(messages)
 print(sent_messages)
