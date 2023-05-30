da9b90e2dfee1c84eed73053a6bb8961395191e6
Marcus Levy <levyma88@gmail.com>
2023-05-30 10:39:35 -0500

    went through examples

---
+++ Chapter 8 (Functions)/pizza.py
@@ -0,0 +1,35 @@
+# passing an arbitary number of arguments
+
+def make_pizza(*toppings):
+    """Print the list of toppings that have been requested."""
+    print(toppings)
+
+
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
+
+
+# make a loop that rund through the toppings
+
+def make_pizza(*toppings):
+    """summarize the pizzas we are about to make."""
+    print("\nMaking a pizza with the following toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
+
+
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra chese')
+
+
+# mixing positional and arbitrary arguments
+
+def make_pizza(size, *toppings):
+    """summarize the pizza we are about to make."""
+    print(f"\nMaking a {size}-inch pizza with the following toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
+
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
