af7493ec2c81cf26d05857fa963b1232f267c554
Marcus Levy <levyma88@gmail.com>
2023-06-08 10:27:53 -0500

    created make pizza examples

---
+++ Chapter 8 (Functions)/make_pizza2.py
@@ -0,0 +1,6 @@
+# import specific function
+
+from pizza import make_pizza
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'yelllow onion', 'sausage')
---
+++ Chapter 8 (Functions)/make_pizzas.py
@@ -0,0 +1,8 @@
+# importing an entire module
+
+import pizza
+
+pizza.make_pizza(16, 'pepperoni')
+pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+# imporint specific functions
