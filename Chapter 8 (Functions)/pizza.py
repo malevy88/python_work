6822e1ce4b240574976ed5c86c8c2b0347eb40b9
Marcus Levy <levyma88@gmail.com>
2023-06-08 10:27:47 -0500

    created import function examples

--- Chapter 8 (Functions)/pizza.py
+++ Chapter 8 (Functions)/pizza.py
@@ -1,26 +1,3 @@
-# passing an arbitary number of arguments
-
-def make_pizza(*toppings):
-    """Print the list of toppings that have been requested."""
-    print(toppings)
-
-
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
-
-
-# make a loop that rund through the toppings
-
-def make_pizza(*toppings):
-    """summarize the pizzas we are about to make."""
-    print("\nMaking a pizza with the following toppings:")
-    for topping in toppings:
-        print(f"- {topping}")
-
-
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra chese')
-
 
 # mixing positional and arbitrary arguments
 
@@ -29,7 +6,3 @@ def make_pizza(size, *toppings):
     print(f"\nMaking a {size}-inch pizza with the following toppings:")
     for topping in toppings:
         print(f"- {topping}")
-
-
-make_pizza(16, 'pepperoni')
-make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
