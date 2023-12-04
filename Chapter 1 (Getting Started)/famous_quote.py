ae4c49ac5b5098bf345e91364b45cd294a91bddb
malevy <levyma88@gmail.com>
2023-12-04 10:51:05 -0600

    Added famous quote 2

--- Chapter 1 (Getting Started)/famous_quote.py
+++ Chapter 1 (Getting Started)/famous_quote.py
@@ -1,2 +1,5 @@
 famous_quote = "Ron Swanson once said 'Don't half ass two things. Whole ass one thing' "
 print(famous_quote)
+
+famous_quote_two = 'do I still know what I am doing at all?'
+print(famous_quote_two)
