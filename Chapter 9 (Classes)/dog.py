6965245eff6bd0e8553527e997e734a632e17d89
malevy <levyma88@gmail.com>
2023-12-04 10:40:09 -0600

    commit

--- Chapter 9 (Classes)/dog.py
+++ Chapter 9 (Classes)/dog.py
@@ -5,3 +5,17 @@ class Dog:
         """Initialize name and age attributes."""
         self.name = name
         self.age = age
+
+    def sit(self):
+        """simulate a dog sitting in response to a command."""
+        print(f"{self.name} is now sitting.")
+
+    def roll_over(self):
+        """simulate rolling over in response to a command."""
+        print(f"{self.name} rolled over!")
+
+
+my_dog = Dog('Willie', 6)
+
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog is {my_dog.age} years old.")
